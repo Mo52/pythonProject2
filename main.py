7d455ca7accd7c86210db805a123cd5ab799bacf
Mo Islam <s65hawkat@gmail.com>
2023-04-16 20:44:14 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+## Declare a variable and initialize it
+f = 101
+print (f)
+# Global vs. local variables in functions
+def someFunction():
+# global f
+    f = "I am learning Python"
+    print(f)
+someFunction()
+print( f)
+
+
+a = "Guru"
+
+print(a)
+
